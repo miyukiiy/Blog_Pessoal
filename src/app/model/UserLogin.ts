97846f1afb7cda59f7cc3ed764b874c2b69c1b6d
Jessica <jehssicamiyuki@gmail.com>
2021-09-30 16:06:32 -0300

    comsumo login cadastro 30.09

---
+++ src/app/model/UserLogin.ts
@@ -0,0 +1,9 @@
+export class UserLogin {
+    public foto: string;
+    public id: number;
+    public nome: string;
+    public senha: string;
+    public tipo: string;
+    public token: string;
+    public usuario: string;
+  }
